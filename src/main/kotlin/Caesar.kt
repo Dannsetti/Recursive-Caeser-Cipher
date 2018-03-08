f35638e7a3157676a8ea3bd8278d89dd6145444c
Daniel Sette <dansetti@gmail.com>
2018-03-08 19:43:20 +0000

    Decipher Function
    Our first solution for the Decipher function

--- src/main/kotlin/Caesar.kt
+++ src/main/kotlin/Caesar.kt
@@ -33,7 +33,70 @@ class Caesar {
     }
 
     fun decipher(s: String): String {
-        TODO()
+        // Letters frequency took from Wikipedia
+        val frequencyAlphabet = "etaoinshrdlcumwfgypbvkjxqz"
+
+        // E is the most frequent and its index is 5 in the alphabet
+        val letterEIndex = 5
+
+        // Create an alphabet to get letter index
+        val alphabet = "abcdefghijklmnopqrstuvwxyz"
+        println(alphabet)
+
+        // the key is the length of the alphabet plus the index of the most frequent
+        // as we are decrypting we need to go backwards so the index of the the letter with
+        // most occurrences will be deducted from the key.
+        var key = 26 + letterEIndex
+        println(key)
+
+        // Create 2 var to hold the key and value of the most frequent letter
+        var letterMaxOccur = ""
+        var letterCounter = 0
+
+        // Group the enciphered string to the numbers of occurrences of each char
+        // to get the most frequent that could be letter E or e in accordance to
+        // the probability of the use of the letter in English words
+        val mostFrequentLetters = s.toLowerCase().groupingBy{it}.eachCount()
+        println(mostFrequentLetters)
+
+        // iterate through the group to get the letter that occurs more
+        for (i in mostFrequentLetters) {
+            if (i.key.toString() in "a".."z") {
+                if (letterCounter < i.value ) {
+                    letterCounter = i.value
+                    letterMaxOccur = i.key.toString()
+                }
+            }
+        }
+
+        var extraIndex = 0
+
+        // counter to get the index in English alphabet of the letter captured in the group
+        var alphabetCounter = 0
+        for (j in alphabet) {
+            alphabetCounter += 1
+            //println("al " + alphabetCounter)
+            // when find it subtract the value index to discover the key that was used to encrypt
+            if (j.toString() == letterMaxOccur) {
+                key -= alphabetCounter
+                println(alphabetCounter)
+                println(key)
+
+            }
+        }
+
+        if (key > 26) {
+            for (k in 26..key) {
+                println("Here")
+                extraIndex += 1
+            }
+            key = extraIndex
+        }
+        
+
+        // call the encipher function to decipher using the key discovered.
+        return encipher(s, key)
+
     }
 }
 
