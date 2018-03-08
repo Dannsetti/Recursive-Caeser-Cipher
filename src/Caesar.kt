aeee204cd207f4877d0ce7aaece18f5a58dc507a
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-03-08 19:15:02 +0000

    Finished encipher function.

--- src/Caesar.kt
+++ src/Caesar.kt
@@ -1,7 +1,35 @@
 class Caesar {
 
     fun encipher(s: String, n: Int): String {
-        TODO()
+        // start reading message string, one char at a time:
+        var currentChar = s[0]
+        // create arraylist to hold result:
+        val result = arrayListOf<Any>()
+        // check curentChar is an uppercase letter:
+        if (currentChar in 'A'..'Z') {
+            // add value of n
+            currentChar += n
+            // rotate if new value is greater than value of 'Z'
+            if(currentChar > 'Z') {
+                currentChar -= 26
+            }
+            // now check if currentChar is a lowercase letter:
+        } else if (currentChar in 'a'..'z') {
+            // add value of n
+            currentChar += n
+            // rotate if new value is greater than value of 'z'
+            if(currentChar > 'z') {
+                currentChar -= 26
+            }
+        }
+        // add curentChar to result
+        result += currentChar
+        // if currentChar is not the final char, call encipher recursively
+        if (s.length > 1) {
+            result += (encipher(s.substring(1), n))
+        }
+        // return result as a String
+        return result.joinToString("")
     }
 
     fun decipher(s: String): String {
