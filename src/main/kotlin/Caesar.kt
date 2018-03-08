e28677a1d78fcf277d022abc35cb2f756d33064c
Dennis Bueso-Calix <dlbcalix@gmail.com>
2018-03-08 22:25:01 +0000

    Further cleanup of code.

--- src/main/kotlin/Caesar.kt
+++ src/main/kotlin/Caesar.kt
@@ -9,7 +9,7 @@ class Caesar {
         var currentChar = s[0]
         // create arraylist to hold result:
         val result = arrayListOf<Any>()
-        // check curentChar is an uppercase letter:
+        // check currentChar is an uppercase letter:
         if (currentChar in 'A'..'Z') {
             // add value of n
             currentChar += n
@@ -26,7 +26,7 @@ class Caesar {
                 currentChar -= 26
             }
         }
-        // add curentChar to result
+        // add currentChar to result
         result += currentChar
         // if currentChar is not the final char, call encipher recursively
         if (s.length > 1) {
@@ -45,13 +45,13 @@ class Caesar {
 
         // Create an alphabet to get letter index
         val alphabet = "abcdefghijklmnopqrstuvwxyz"
-        //println(alphabet)
+        // println(alphabet)
 
         // the key is the length of the alphabet plus the index of the most frequent
         // as we are decrypting we need to go backwards so the index of the the letter with
         // most occurrences will be deducted from the key.
         var key = 26 + letterEIndex
-        //println(key)
+        // println(key)
 
         // Create 2 var to hold the key and value of the most frequent letter
         var letterMaxOccur = ""
@@ -61,7 +61,7 @@ class Caesar {
         // to get the most frequent that could be letter E or e in accordance to
         // the probability of the use of the letter in English words
         val mostFrequentLetters = s.toLowerCase().groupingBy { it }.eachCount()
-        //println(mostFrequentLetters)
+        // println(mostFrequentLetters)
 
         // iterate through the group to get the letter that occurs more
         for (i in mostFrequentLetters) {
@@ -79,28 +79,26 @@ class Caesar {
         var alphabetCounter = 0
         for (j in alphabet) {
             alphabetCounter += 1
-            //println("al " + alphabetCounter)
+            // println("al " + alphabetCounter)
             // when find it subtract the value index to discover the key that was used to encrypt
             if (j.toString() == letterMaxOccur) {
                 key -= alphabetCounter
-                //println(alphabetCounter)
-                //println(key)
+                // println(alphabetCounter)
+                // println(key)
 
             }
         }
 
         if (key > 26) {
             for (k in 26..key) {
-                //println("Here")
+                // println("Here")
                 extraIndex += 1
             }
             key = extraIndex
         }
 
-
         // call the encipher function to decipher using the key discovered.
         return encipher(s, key)
-
     }
 
     fun decipher2(s: String): String {
@@ -123,7 +121,6 @@ class Caesar {
                 if (frequentWords.contains(j)) {
                     found = true
                 }
-
             }
             // if there is a match, add decrypted sample to possible solution
             if (found) {
@@ -136,10 +133,10 @@ class Caesar {
         return decrypted
     }
 
-    fun createWordList(): ArrayList<String> {
+    private fun createWordList(): ArrayList<String> {
         // create and load dictionary array of frequent words
-        val PATH = System.getProperty("user.dir") + "/src/main/resources/"
-        val input = Scanner(File(PATH + "100FrequentWords.txt"))
+        val path = System.getProperty("user.dir") + "/src/main/resources/"
+        val input = Scanner(File(path + "100FrequentWords.txt"))
         val frequentWords = ArrayList<String>()
         var line = ""
         while (input.hasNext()) {
@@ -148,26 +145,23 @@ class Caesar {
         }
         return frequentWords
     }
-
 }
 
 fun main(args: Array<String>) {
     val encrypt = Caesar().encipher("Caesar cipher? I prefer Caesar salad.", 25)
-    println("Encrypt: " + encrypt)
+    println("Encrypt: $encrypt")
 
     val decrypt1 = Caesar().decipher("Hu lkbjhapvu pz doha ylthpuz hmaly dl mvynla clyfaopun dl " +
             "ohcl slhyulk.")
-    println("Decrypt: " + decrypt1)
+    println("Decrypt: $decrypt1")
 
     val decrypt2 = Caesar().decipher("Bzdrzq bhogdq? H oqdedq Bzdrzq rzkzc.")
-    println("Decrypt: " + decrypt2)
+    println("Decrypt: $decrypt2")
 
     val decrypt3 = Caesar().decipher2("Hu lkbjhapvu pz doha ylthpuz hmaly dl mvynla clyfaopun dl " +
             "ohcl slhyulk.")
-    println("Decrypt2: " + decrypt3)
+    println("Decrypt2: $decrypt3")
 
     val decrypt4 = Caesar().decipher2("Bzdrzq bhogdq? H oqdedq Bzdrzq rzkzc.")
-    println("Decrypt2: " + decrypt4)
-
-
+    println("Decrypt2: $decrypt4")
 }
